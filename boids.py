654b35b6009756ecd0a0837b17bb6860695ef413
Job van Ellen <jobvanellen@kpnmail.nl>
2020-12-16 14:56:52 +0100

    cohesion implementd, static axis size

--- boids.py
+++ boids.py
@@ -7,18 +7,17 @@ import matplotlib.pyplot as plt
 #position updates: new_pos = old_pos + speed*dt
 #speed updates: new_speed = old_speed + accel*dt
 
+#optional: turn rules on and off during runtime
+
 # simulation settings
 x_size = 10
 y_size = 10
 z_size = 10
 num_boids = 50
-time = 1000 #seconds
-dt = 0.1 #seconds
+time = 100 #seconds
+dt = 1 #seconds
 
 boids = []
-
-# visual setup
-
 
 # defining our boids
 class Boid:
@@ -37,8 +36,10 @@ def init_boids():
 def draw_boids():
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+    ax.set(xlim=(0,x_size), ylim=(0,y_size), zlim=(0,z_size))
     for b in boids:
-        ax.quiver(b.position[0], b.position[1], b.position[2],b.velocity[0],b.velocity[1],b.velocity[2])
+        ax.quiver(b.position[0], b.position[1], b.position[2],\
+            b.velocity[0],b.velocity[1],b.velocity[2], length=.5, normalize=True)
 
     plt.show()
 
@@ -48,7 +49,7 @@ def move_boids():
         v1 = cohesion(b)
         v2 = separation(b)
         v3 = alignment(b)
-
+        b.velocity = b.velocity + v1 + v2 + v3
         b.position = b.position + b.velocity*dt
         for i in range(3):
             if b.position[i] >= 10.0:
@@ -58,16 +59,25 @@ def move_boids():
 
 # basic ruleset
 # boids try to fly to the centre of mass of neighbouring boids
-def cohesion(boid):
-    pass
+def cohesion(current_boid):
+    vector = np.array([0,0,0])
+    for b in boids:
+        if b != current_boid:
+            vector = vector + b.position
+        vector = vector/(len(boids)-1)
+    return vector
 
 # boids try not to collide with other boids
 def separation(boid):
-    pass
+    vector = np.array([0,0,0])
+    
+    return vector
 
 # boids try to match velocity/direction with near boids
 def alignment(boid):
-    pass
+    vector = np.array([0,0,0])
+    
+    return vector
 
 # extended ruleset
 
